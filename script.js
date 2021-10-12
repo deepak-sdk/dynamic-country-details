9f03966f6bf1593a3cd36308cb5e251eba695e24
Deepak <71409090+deepak-sdk@users.noreply.github.com>
2021-10-12 05:51:23 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ xhr.onload = () => {
   const allcountrieshere = xhr.response;
 
   // a.   Get all the countries from Asia continent /region using Filter function
-
-  var Asia = allcountrieshere.filter((country) => country.region == "Asia");
-  //   console.log(Asia);
-  Object.entries(Asia).forEach((entry) => {
+  let population = 200000;
+  let populationMoreThan2lakhs = allcountrieshere.filter(
+    (country) => country.population > population
+  );
+  console.log(populationMoreThan2lakhs);
+  Object.entries(populationMoreThan2lakhs).forEach((entry) => {
     const [key, value] = entry;
     // console.log(value.flags.png);
     // console.log(value.name.common);
-
+    // console.log(value.population);
+    // console.log(value.region);
 
     // create a new div element
     const countryCard = document.createElement("div");
@@ -35,15 +38,30 @@ xhr.onload = () => {
 
     // adding element to countryDetail
     const countryName = document.createElement("h4");
-    countryName.setAttribute("style","color:white");
-    countryName.innerHTML = `${value.name.common}`;
+    countryName.textContent = `${value.name.common}`;
+
+    const countryPopulation = document.createElement("p");
+    countryPopulation.setAttribute("class", "country-population");
+    countryPopulation.textContent = `${value.population}`;
+
+    const countryRegion = document.createElement("p");
+    countryRegion.setAttribute("class", "country-region");
+    countryRegion.textContent = `${value.region}`;
+
+    const countryCapital = document.createElement("p");
+    countryCapital.setAttribute("class", "country-capital");
+    countryCapital.textContent = `${value.capital}`;
 
     countryDetails.appendChild(countryName);
-
-
-    
-
+    countryDetails.appendChild(countryPopulation);
+    countryDetails.appendChild(countryRegion);
+    countryDetails.appendChild(countryCapital);
     // append to parent element in html
+
+    // const countryCards = document.createElement("div");
+    // countryCards.setAttribute("class", "countryCards");
+    // countryCards.appendChild(countryCard);
+
     document.querySelector(".countryCards").append(countryCard);
   });
 };
